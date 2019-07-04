b3bd624dc28eb149516504642c1c844d6543d275
Akos Kolloros <kollorosa@gmail.com>
2019-07-04 15:12:55 +0200

    websocket works

---
+++ src/UserComponent.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+class UserComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: []
+        };
+    }
+
+    setupWebSocket(){
+        let webSocketInstance = this;
+        let ws = new WebSocket('ws://localhost:8080/user');
+        ws.onerror = function(event){
+            console.log('error!');
+            console.log(event)
+        };
+        ws.onopen =function(event){
+            console.log('onopen!');
+        };
+        ws.onmessage = function(event){
+            console.log('onmessage!');
+            webSocketInstance.handleData(event.data)
+        };
+        ws.onclose = function(event){
+            console.log('onclose');
+            setTimeout(webSocketInstance.setupWebSocket, 1000);
+        };
+    }
+
+    handleData(data) {
+        console.log(data);
+        let result = JSON.parse(data);
+        this.state.users.push(result);
+        // this.setState({users: result});
+    }
+
+    render() {
+        this.setupWebSocket();
+        return (
+            <div>
+                Count: <strong>{this.state.users}</strong>
+            </div>
+        );
+    }
+}
+
+export default UserComponent;
